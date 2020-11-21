46041ab7a2d2f35da22c1c473da4475f2ede6ec0
huangweijie <949801851@qq.com>
2020-11-21 11:24:54 +0800

    推送遗漏代码

---
+++ src/views/profile/MyFocus.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="myFocus">
+    <headerTemplate title="我的关注" @click="back" />
+    <focusUserTemplate nickName="流连忘返" />
+  </div>
+</template>
+
+<script>
+import headerTemplate from "@/components/HeaderTemplate";
+import focusUserTemplate from "@/components/FocusUserTemplate";
+export default {
+  components: {
+    headerTemplate,
+    focusUserTemplate,
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+#myFocus {
+  background-color: #f2f2f2;
+  min-height: 100vh;
+}
+</style>
---
+++ src/views/profile/UserEdit.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="userEdit">
+    <headerTemplate title="编辑资料" @click="back" />
+    <div class="userPic">
+      <img src="@/assets/默认头像.png" alt="" />
+    </div>
+    <toSetTemplate title="昵称" description="火星网友" />
+    <toSetTemplate title="密码" description="******" />
+    <toSetTemplate title="性别" description="男" />
+  </div>
+</template>
+
+<script>
+import headerTemplate from "@/components/HeaderTemplate";
+import toSetTemplate from "@/components/ToSetTemplate";
+export default {
+  components: {
+    headerTemplate,
+    toSetTemplate,
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+#userEdit {
+  background-color: #f2f2f2;
+  min-height: 100vh;
+  .userPic {
+    width: 70 / 360 * 100vw;
+    height: 70 / 360 * 100vw;
+    border-radius: 35 / 360 * 100vw;
+    margin: 12vw auto;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
