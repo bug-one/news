fe70a7e1949b95d4b31229700eb7b1f0b145266e
huangweijie <949801851@qq.com>
2020-11-21 10:31:03 +0800

    新增我的关注页

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,9 @@ import VueRouter from 'vue-router'
 import Index from '../views/Index'
 import Login from '../views/Login'
 import Register from '../views/Register.vue'
-import User from '../views/User.vue'
-import UserEdit from '../views/UserEdit.vue'
+import User from '../views/profile/User.vue'
+import UserEdit from '../views/profile/UserEdit.vue'
+import MyFocus from '../views/profile/MyFocus.vue'
 
 
 Vue.use(VueRouter)
@@ -15,7 +16,8 @@ const routes = [
   { path: '/login', component: Login },
   { path: '/register', component: Register },
   { path: '/user', component: User },
-  { path: '/userEdit', component: UserEdit }
+  { path: '/userEdit', component: UserEdit },
+  { path: '/myFocus', component: MyFocus },
 ]
 
 const router = new VueRouter({
