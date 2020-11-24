d997ee5db9cd52dd7108f5e97045b3388b13b691
huangweijie <949801851@qq.com>
2020-11-24 15:27:32 +0800

    获取分类列表

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="index">
     <IndexHeader />
+    <van-tabs v-model="active">
+      <van-tab
+        :title="item.name"
+        v-for="(item, index) in categoryList"
+        :key="item.id"
+        >内容{{ index + 1 }}</van-tab
+      >
+    </van-tabs>
   </div>
 </template>
 
@@ -10,10 +18,26 @@ export default {
   components: {
     IndexHeader,
   },
+  data() {
+    return {
+      active: 0,
+      categoryList: [],
+    };
+  },
   methods: {
     toUser() {
       this.$router.push("user").catch((err) => {});
     },
+  },
+  created() {
+    this.$axios({
+      url: "/category",
+    }).then((res) => {
+      if (res.status == 200) {
+        this.categoryList = res.data.data;
+        console.log(this.categoryList);
+      }
+    });
   },
 };
 </script>
