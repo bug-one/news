deb025d9c8316bf9c38db7cb12a7f18c0215b5fd
huangweijie <949801851@qq.com>
2020-11-24 19:50:05 +0800

    优化主页样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="index">
     <IndexHeader />
-    <van-tabs v-model="active">
+    <van-tabs
+      v-model="active"
+      background="#e9e9e9"
+      class="nav"
+      line-width="8vw"
+    >
       <van-tab :title="item.name" v-for="item in categoryList" :key="item.id">
         <PostList v-for="list in postList" :key="list.id" :list="list" />
       </van-tab>
+      <div class="mask">
+        <div class="iconfont iconjiantou"></div>
+      </div>
     </van-tabs>
   </div>
 </template>
@@ -50,4 +58,36 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.index {
+  min-height: 100vh;
+  background-color: #f2f2f2;
+}
+/deep/ .van-tabs--line .van-tabs__wrap {
+  height: 10vw;
+  .van-tab__text {
+    font-size: 12 / 360 * 100vw;
+  }
+}
+.mask {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 20vw;
+  height: 10vw;
+  overflow: hidden;
+}
+.iconjiantou {
+  position: absolute;
+  top: 0;
+  right: 0;
+  font-size: 12 / 360 * 100vw;
+  color: #777;
+  transform: rotate(-90deg);
+  background-color: #e9e9e9;
+  width: 10vw;
+  height: 10vw;
+  line-height: 10vw;
+  text-align: center;
+  box-shadow: 0vw -3vw 5vw 7px #e9e9e9;
+}
 </style>
