dfae02ca3b3ac6a9950a2922d896448fb8c8b3ef
huangweijie <949801851@qq.com>
2020-11-27 10:30:54 +0800

    提交遗漏代码

---
+++ src/views/profile/Star.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="star">
+    <headerTemplate title="我的收藏" />
+    <postList :list="list" v-for="list in postList" :key="list.id" />
+  </div>
+</template>
+
+<script>
+import headerTemplate from "../../components/HeaderTemplate";
+import postList from "../../components/PostList";
+
+export default {
+  data() {
+    return {
+      postList: [],
+    };
+  },
+  components: {
+    headerTemplate,
+    postList,
+  },
+  created() {
+    this.$axios({
+      url: "/user_star",
+    }).then((res) => {
+      if (res.status == 200) {
+        this.postList = res.data.data;
+      }
+    });
+  },
+};
+</script>
+
+<style lang="less" scoped>
+#star {
+  background-color: #f2f2f2;
+  min-height: 100vh;
+}
+</style>
